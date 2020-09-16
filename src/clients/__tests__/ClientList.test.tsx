d5ac107ed5900eacd5fb5e587154c5887667ce5b
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-09-16 17:33:15 +0200

    fix test

--- src/clients/__tests__/ClientList.test.tsx
+++ src/clients/__tests__/ClientList.test.tsx
@@ -3,18 +3,13 @@ import { MemoryRouter } from "react-router-dom";
 import { render } from "@testing-library/react";
 
 import clientMock from "./mock-clients.json";
-import { I18nextProvider } from "react-i18next";
-import i18n from "../../i18n";
 import { ClientList } from "../ClientList";
 
 test("renders ClientList", () => {
-  const { getByText } = render(
-    <I18nextProvider i18n={i18n}>
-      <MemoryRouter>
-        <ClientList clients={clientMock} baseUrl="http://blog.nerdin.ch" />
-      </MemoryRouter>
-    </I18nextProvider>
+  const container = render(
+    <MemoryRouter>
+      <ClientList clients={clientMock} baseUrl="http://blog.nerdin.ch" />
+    </MemoryRouter>
   );
-  const headerElement = getByText(/Client ID/i);
-  expect(headerElement).toBeInTheDocument();
+  expect(container).toMatchSnapshot();
 });
